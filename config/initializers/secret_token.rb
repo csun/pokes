d0e6a2566888cca6260cc598b082ff83bee38940
Cameron Sun <cameron.csun@gmail.com>
2013-10-24 10:59:49 -0400

    Initial commit with empty rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pokes::Application.config.secret_key_base = 'd5b081d722ac7a211bf0563e0895e9d9df01c75267c3462b77ffb4797db3f07cbe067cad8c0aa474defaf0ed6c78924a5f2997ee8a5c4e1fe8357e80acd9004b'
