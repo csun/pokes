bc7b9a93d2a553c4562df80ddb99758179a0c59e
Cameron Sun <cameron.csun@gmail.com>
2013-11-06 16:11:28 -0500

    Finished last test on logout, moving on to pokes

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,16 +1,34 @@
 require 'spec_helper'
 
 describe SessionsController do
-	describe "POST /sessions" do
-		context "with a valid login attempt" do
+	describe 'POST /sessions' do
+		context 'with a valid login attempt' do
 			before do
 				@user = FactoryGirl.create(:user)
-				post :create, username: @user.username, password: @user.password
+				login_as @user
 			end
 
-			it "sets user_id in session" do
+			it 'sets user_id in session' do
 				session[:user_id].should eq @user.id
 			end
 		end
 	end
+
+	describe 'GET /logout' do
+		context 'when already logged in' do
+			before do
+				@user = FactoryGirl.create(:user)
+				login_as @user
+			end
+
+			it 'sets user_id in session back to nil' do
+				get :destroy
+				session[:user_id].should be_nil
+			end
+		end
+	end
+end
+
+def login_as(user)
+	post :create, username: @user.username, password: @user.password
 end
